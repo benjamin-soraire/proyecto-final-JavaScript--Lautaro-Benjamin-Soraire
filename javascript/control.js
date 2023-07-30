3a23d07f79565a38f1ef4025a5fda1b463c7e182
benjaminsoraire <benjamingionenotti@gmail.com>
2023-07-30 18:57:06 -0300

    version 2

--- javascript/control.js
+++ javascript/control.js
@@ -1,34 +1,19 @@
-let TotalDeCompra = 0;
-let mueble = prompt('1-Sofá cama... $15000\n2-Mesa de roble tallada... $75000\n3-Escritorio de madera pulida... $60000\n4-asiento estilo Otomanas... $32000\npulse 0 para salir');
+console.table(muebles)
 
-while (mueble != "0"){
-    switch(mueble){
-        case '1':
-            alert('agregaste Sofá cama a tu canasta $15000');
-            sumarTotalDeCompra(15000);
-            break;
-        case '2':
-            alert('agregaste Mesa de roble tallada $75000');
-            sumarTotalDeCompra(75000);
-            break;
-        case '3':
-            alert('agregaste Escritorio de madera pulida $60000');
-            sumarTotalDeCompra(60000);
-            break;
-        case '4':
-            alert('agregaste asiento estilo Otomanas $32000');
-            sumarTotalDeCompra(32000);
-            break;
-        default:
-            alert("codigo incorrecto o no disponible");
-            break;
+function filtrarPorPrecio(precioMax){
+    const filtrados = muebles.filter((muebles) =>  muebles.precio <= precioMax);
+    //console.log(filtrados)//
+    return filtrados
+}	
+
+let precioMueble = parseFloat(prompt("ingresar precio maximo (0-salir)"))
+while(precioMueble != 0){
+    if(isNaN(precioMueble) || precioMueble < 0){
+        alert("ingresa un valor valido");
     }
-    mueble = prompt('1-Sofá cama... $15000\n2-Mesa de roble tallada... $75000\n3-Escritorio de madera pulida... $60000\n4-asiento estilo Otomanas... $32000\npulse 0 para salir');
-}
-
-alert("el valor de tu canasta es $" +TotalDeCompra);
-
-function sumarTotalDeCompra(_precioMueble){
-    TotalDeCompra = TotalDeCompra + _precioMueble;
-    console.log("subtotal de tu compra $"+TotalDeCompra)
+    else{
+        const mueblesFiltrados = filtrarPorPrecio(precioMueble);
+    console.table(mueblesFiltrados);
+    }
+    precioMueble = parseFloat(prompt("ingresar precio maximo (0-salir)"));
 }
---
+++ javascript/items.js
@@ -0,0 +1,72 @@
+const muebles =[
+    {
+        "id": 1,
+        "nombre": "Sofá Elegante",
+        "descripcion": "Sofisticación y comodidad combinadas.",
+        "precio": 19.99,
+        "imagen": "Sofa-Elegante.jpg"
+    },
+    {
+        "id": 2,
+        "nombre": "Mesa de Roble Rústica",
+        "descripcion": "Estilo natural y duradero",
+        "precio": 24.99,
+        "imagen": "Mesa-de-Roble-Rústica.jpg"
+    },
+    {
+        "id": 3,
+        "nombre": "Silla Ergonómica",
+        "descripcion": "silla-ergonómica.jpg",
+        "precio": 14.95,
+        "imagen": ""
+    },
+    {
+        "id": 4,
+        "nombre": "Cama Acolchada Deluxe",
+        "descripcion": "Descanso de lujo asegurado.",
+        "precio": 29.99,
+        "imagen": "Cama-Acolchada-Deluxe.jpg"
+    },
+    {
+        "id": 5,
+        "nombre": "Cómoda Vintage ",
+        "descripcion": "Encanto retro para tu hogar.",
+        "precio": 9.99,
+        "imagen": "Cómoda-Vintage.jpg"
+    },
+    {
+        "id": 6,
+        "nombre": "Escritorio Multifuncional ",
+        "descripcion": "Espacio de trabajo versátil.",
+        "precio": 34.99,
+        "imagen": "Escritorio-Multifuncional.jpg"
+    },
+    {
+        "id": 7,
+        "nombre": "Estantería Minimalista",
+        "descripcion": "Orden y diseño moderno.",
+        "precio": 17.50,
+        "imagen": "Estanteria-Minimalista.jpg"
+    },
+    {
+        "id": 8,
+        "nombre": "Sillón Reclinable Relax",
+        "descripcion": "Relajación al máximo nivel.",
+        "precio": 12.49,
+        "imagen": "Sillón-Reclinable-Relax.jpg"
+    },
+    {
+        "id": 9,
+        "nombre": "Mesa de Centro Contemporánea",
+        "descripcion": "Elegancia en tu salón.",
+        "precio": 39.95,
+        "imagen": "Mesa-de-Centro-Contemporánea.jpg"
+    },
+    {
+        "id": 10,
+        "nombre": "Armario Organizador Espacioso",
+        "descripcion": "Armario Organizador Espacioso",
+        "precio": 23.75,
+        "imagen": "Armario-Organizador-Espacioso.jpg"
+    }
+]
